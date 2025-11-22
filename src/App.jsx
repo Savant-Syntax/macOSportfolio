dd2def5da8488c6ca8fb6fc1e907159ee1c65ed7
Savant-Syntax <vinitastrak@gmail.com>
2025-11-22 20:54:56 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Navbar from "#components/Navbar"
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Welcome to my MacFolio</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
